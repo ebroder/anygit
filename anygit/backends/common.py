dc45bdaca1b678f128ae76210fe02d71b996d6ec
Greg Brockman <gdb@mit.edu>
2010-03-30 01:55:52 -0400

    Improvements to the common API

--- anygit/backends/common.py
+++ anygit/backends/common.py
@@ -1,4 +1,11 @@
+import hashlib
+import re
 import urlparse
+
+from anygit.data import exceptions
+
+def sha1(string):
+    return hashlib.sha1(string).hexdigest()
 
 class CommonMixin(object):
     """Functionality common to all backends."""
@@ -13,6 +20,13 @@ class CommonMixin(object):
         instance.save()
         return instance
 
+    @classmethod
+    def get_or_create(cls, **kwargs):
+        try:
+            return cls.get_by_attributes(**kwargs)
+        except exceptions.DoesNotExist:
+            return cls.create(**kwargs)
+
     def error(self, attr, msg):
         self._errors.setdefault(attr, []).append(msg)
 
@@ -21,6 +35,11 @@ class CommonMixin(object):
 
 
 class CommonRepositoryMixin(CommonMixin):
+    @classmethod
+    def create(cls, url):
+        id = sha1(url)
+        return super(CommonRepositoryMixin, cls).create(id=id, url=url)
+
     def __str__(self):
         return "Repository: %s" % self.id
     __repr__ = __str__
@@ -33,12 +52,17 @@ class CommonRepositoryMixin(CommonMixin):
             self.error("url", "Must provide a url")
 
     @property
+    def _parsed_url(self):
+        stripped_url = re.sub('^[^:]*:', '', self.url)
+        return urlparse.urlparse(stripped_url)
+
+    @property
     def host(self):
-        return urlparse.urlparse(self.url).netloc
+        return self._parsed_url.netloc
 
     @property
     def path(self):
-        return urlparse.urlparse(self.url).path
+        return self._parsed_url.path
 
 
 class CommonRemoteHeadMixin(CommonMixin):
