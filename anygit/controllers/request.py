c32740cc2fce4bfdc45466088a6113b5e044014d
Greg Brockman <gdb@mit.edu>
2010-05-16 00:38:37 -0400

    Added logic to the request processing page

--- anygit/controllers/request.py
+++ anygit/controllers/request.py
@@ -3,9 +3,10 @@ import logging
 from pylons import request, response, session, tmpl_context as c
 from pylons.controllers.util import abort, redirect_to
 
-from anygit.lib.base import BaseController, render
-from anygit.data import exceptions
 from anygit import models
+from anygit.data import exceptions
+from anygit.lib.base import BaseController, render
+from anygit.lib import helpers
 
 log = logging.getLogger(__name__)
 
@@ -14,6 +15,22 @@ class RequestController(BaseController):
         return render('/request/index.mako', controller='request')
 
     def do_request(self):
-        if not models.Repository.exists(url=request.params['url']):
-            models.Repository.create(url=request.params['url'])
-            models.flush()
+        url = request.params.get('url')
+        if not url:
+            helpers.error('You did not provide a URL')
+            redirect_to('index')
+
+        if models.Repository.exists(url=url):
+            helpers.error('Someone has already requested indexing of %s' % url)
+            redirect_to('index')
+
+        models.Repository.create(url=url)
+        models.flush()
+
+        if url.startswith('git://'):
+            helpers.flash('Successfully requested %s for indexing' % url)
+        else:
+            helpers.flash('Successfully requested %s for indexing.  However, '
+                          'please note that only git protocol (git://) '
+                          'repositories are currently supported by anygit.' % url)
+        redirect_to('index')
