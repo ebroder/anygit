8382e003beab75b2899500e790d6dc50e24c1d9b
Greg Brockman <gdb@mit.edu>
2010-05-22 19:21:34 -0400

    Fixup with more 32 bit support

--- anygit/__init__.py
+++ anygit/__init__.py
@@ -1,8 +1,13 @@
+import glob
 import os
 import sys
 
-DIR = os.path.dirname(__file__)
+DIR = os.path.abspath(os.path.dirname(__file__))
+
+matches = []
+for mongo_dir in ['../pkgs/lib/python', '../pkgs/lib64/python']:
+    matches += glob.glob(os.path.join(DIR, mongo_dir, 'pymongo-1.6_-py2.6-*'))
+
 # Giant hack.  TODO: cleanup
-__LOCAL_MODULES = os.path.abspath(os.path.join(DIR, '../pkgs/lib64/python/pymongo-1.6_-py2.6-linux-x86_64.egg/'))
-if __LOCAL_MODULES not in sys.path:
-    sys.path.insert(0, __LOCAL_MODULES)
+sys.path[0:0] = matches
+del matches
