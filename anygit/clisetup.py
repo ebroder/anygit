a817afa1580aeb59fcbe837893c9ec8c5e7e0667
Greg Brockman <gdb@mit.edu>
2010-05-21 20:07:37 -0400

    Load the logging config right away so it actually works

--- anygit/clisetup.py
+++ anygit/clisetup.py
@@ -5,6 +5,7 @@ import sys
 
 DIR = os.path.abspath(os.path.dirname(__file__))
 conf = os.path.join(DIR, '../conf/anygit.ini')
+logging.config.fileConfig(conf)
 application = loadapp('config:%s' % conf, relative_to='/')
 app = loadapp('config:%s' % conf,relative_to=os.getcwd())
-logging.config.fileConfig(conf)
+
